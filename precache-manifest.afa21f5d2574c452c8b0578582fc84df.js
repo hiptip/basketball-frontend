91d6f091a410cd9397ba1611eda7f1066c18b848
nboston <nboston@phone.com>
2021-12-29 00:03:47 -0500

    Updates

---
+++ precache-manifest.afa21f5d2574c452c8b0578582fc84df.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaedad36111c79f0e3dfd33eca440e8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56b704e8c0874bc8e253",
+    "url": "/static/css/main.b1e21ffd.chunk.css"
+  },
+  {
+    "revision": "634510edf87ac7db4bf3",
+    "url": "/static/js/2.53bf2f7d.chunk.js"
+  },
+  {
+    "revision": "0e271e56f042b02dbdf0688f9e79afb6",
+    "url": "/static/js/2.53bf2f7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1ccf6793af716274700",
+    "url": "/static/js/3.88da388f.chunk.js"
+  },
+  {
+    "revision": "56b704e8c0874bc8e253",
+    "url": "/static/js/main.b8daaae1.chunk.js"
+  },
+  {
+    "revision": "7e3e3face7a5a14c4bf5",
+    "url": "/static/js/runtime-main.8f8c49e7.js"
+  },
+  {
+    "revision": "551a7c87254138379c8bb15d978552ed",
+    "url": "/static/media/arrow.551a7c87.svg"
+  },
+  {
+    "revision": "3897d9ca144e3eac32a48e41b849c2a1",
+    "url": "/static/media/bg1.3897d9ca.svg"
+  },
+  {
+    "revision": "aaf4a0547f0659131a559d3adfa46148",
+    "url": "/static/media/splash-basketball.aaf4a054.svg"
+  },
+  {
+    "revision": "b5da56a613061aec07fb8a6eccdf10b4",
+    "url": "/static/media/splash-lockup.b5da56a6.svg"
+  }
+]);
